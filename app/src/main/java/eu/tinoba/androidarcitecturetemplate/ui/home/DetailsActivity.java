4eeb2e5839c32df49c509d2790ef1063be722b8e
tinoba <tinbalint@gmail.com>
2017-04-30 20:10:37 +0200

    add new Activity

---
+++ app/src/main/java/eu/tinoba/androidarcitecturetemplate/ui/home/DetailsActivity.java
@@ -0,0 +1,15 @@
+package eu.tinoba.androidarcitecturetemplate.ui.home;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import eu.tinoba.androidarcitecturetemplate.R;
+
+public class DetailsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_details);
+    }
+}
